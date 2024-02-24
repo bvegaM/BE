abc7c3fffd974bf7722ca00f0ade2d71dcd2ac0e
bvegam <vegabryam40@gmail.com>
2024-02-23 21:38:15 -0500

    add cors project

--- BE-Module-15/lesson 2/flask-socketio-chat/app.py
+++ BE-Module-15/lesson 2/flask-socketio-chat/app.py
@@ -1,15 +1,25 @@
 #!/bin/env python
+import json
 from flask import render_template
 from web_socket_server import WebSocketServer, socketio, app
 
 app = WebSocketServer().create_app()
-messages = []
+user_messages = {}
 
 @socketio.on('message')
 def handle_message(message):
+    message = json.loads(message)
     print(f'Received message: {message}')
-    messages.append(message)
-    socketio.emit('message', message)
+    username = message['user']
+    msg = message['message']
+    
+    if username in user_messages:
+        user_messages[username].append(msg)
+    else:
+        user_messages[username] = [msg]
+    
+    socketio.emit('message', message['user']+" "+message['message'])
+
 
 @socketio.on('get_all_messages')
 def handle_get_user_messages():
